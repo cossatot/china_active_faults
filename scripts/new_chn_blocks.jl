67830a5f0c56bf3f561d9c25581da6af195d2858
Richard Styron <richard.h.styron@gmail.com>
2021-12-10 20:24:21 -0800

    cleaned up scripts

--- scripts/new_chn_blocks.jl
+++ scripts/new_chn_blocks.jl
@@ -8,7 +8,7 @@ using DataFrames, DataFramesMeta
 using ArchGDAL
 const AG = ArchGDAL
 
-#using PyPlot
+using PyPlot
 
 save_results = true
 
@@ -155,178 +155,16 @@ if save_results == true
                                            
 end
 
-#pred_geol_slip_rates = []
-#for (i, rate) in enumerate(geol_slip_rate_vels)
-#    fault_idx = parse(Int, rate.name)
-#    fault_row = @where(fault_df, :fid .== fault_idx)[1,:]
-#    fault = Oiler.IO.row_to_fault(fault_row)
-#    
-#    if haskey(poles, (rate.fix, rate.mov))
-#        pred_rate = Oiler.Faults.get_fault_slip_rate_from_pole(fault, 
-#                        poles[(rate.fix, rate.mov)]; lon=rate.lon, lat=rate.lat)
-#    else
-#        pred_rate = Oiler.Faults.get_fault_slip_rate_from_pole(fault,
-#                        poles[(rate.mov, rate.fix)]; lon=rate.lon, lat=rate.lat)
-#    end
-#    
-#    push!(pred_geol_slip_rates, pred_rate)
-#end
-#
-#
-#obs_vels = [v["vel"] for v in Oiler.Utils.get_gnss_vels(vel_groups)]
-#pred_vels = [v["vel"] for v in Oiler.Utils.get_gnss_vels(results["predicted_vels"])]
-#
-#vlon, vlat = Oiler.Utils.get_coords_from_vel_array(obs_vels)
-#ove = [v.ve for v in obs_vels]
-#ovn = [v.vn for v in obs_vels]
-#
-#pve = [v.ve for v in pred_vels]
-#pvn = [v.vn for v in pred_vels]
-#
-#rve, rvn = ove - pve, ovn - pvn
-#
-#pred_gnss_df = DataFrame()
-#pred_gnss_df.lon = vlon
-#pred_gnss_df.lat = vlat
-#pred_gnss_df.ve =  round.(pve, digits=3)
-#pred_gnss_df.vn =  round.(pvn, digits=3) 
-#pred_gnss_df.ee =  round.([v.ee for v in pred_vels], digits=3)
-#pred_gnss_df.en =  round.([v.en for v in pred_vels], digits=3)
-#pred_gnss_df.cen = round.([v.cen for v in pred_vels], digits=3)
-#pred_gnss_df.re =  round.(rve, digits=3)
-#pred_gnss_df.rn =  round.(rvn, digits=3)
-#pred_gnss_df.ree = round.(sqrt.(pred_gnss_df.ee.^2 + [v.ee^2 for v in obs_vels]), digits=3)
-#pred_gnss_df.ren = round.(sqrt.(pred_gnss_df.en.^2 + [v.en^2 for v in obs_vels]), digits=3)
-#
-#if save_results == true
-#    CSV.write("../block_data/pred_gnss.csv", pred_gnss_df)
-#end
-#
-#inc = map(!, iszero.(geol_slip_rate_df[!,:include]))
-#
-#function check_missing(val)
-#    if val == ""
-#        return false
-#    elseif val == 0.
-#        return false
-#    elseif ismissing(val) | isnothing(val)
-#        return false
-#    else
-#        return true
-#    end
-#end
-#
-#dex_inc = [check_missing(d) for d in geol_slip_rate_df[!,:dextral_rate]]
-#ext_inc = [check_missing(d) for d in geol_slip_rate_df[!,:extension_rate]]
-#
-#dex_geol_obs = geol_slip_rate_df[!,:dextral_rate][dex_inc .* inc]
-#dex_geol_err = parse.(Float64, geol_slip_rate_df[!,:dextral_err][dex_inc .* inc])
-#dex_geol_pred = [p[1] for p in pred_geol_slip_rates[dex_inc[inc]]]
-#dex_geol_pred_err = [p[3] for p in pred_geol_slip_rates[dex_inc[inc]]]
-#
-#ext_geol_obs = parse.(Float64, geol_slip_rate_df[!,:extension_rate][ext_inc .* inc])
-#ext_geol_err = parse.(Float64, geol_slip_rate_df[!,:extension_err][ext_inc .* inc])
-#ext_geol_pred =  [p[2] for p in pred_geol_slip_rates[ext_inc[inc]]]
-#ext_geol_pred_err = [p[4] for p in pred_geol_slip_rates[ext_inc[inc]]]
-#
-#all_obs = vcat(ove, ovn, dex_geol_obs, ext_geol_obs)
-#all_errs = vcat([v.ee for v in obs_vels], [v.en for v in obs_vels], 
-#                dex_geol_err, ext_geol_err)
-#
-#all_pred = vcat(pve, pvn, dex_geol_pred, ext_geol_pred)
-#
-#chi_sq = sum(((all_obs .- all_pred).^2 ) ./ all_errs.^2)
-#
-#n_param = length(keys(vel_groups)) / 3.
-#
-#red_chi_sq = chi_sq / (length(all_obs) - n_param)
-#
-#
-#println("Reduced Chi Sq: $red_chi_sq")
-#
-#
-#figure(figsize=(14, 10))
-#for fault in faults
-#    plot(fault.trace[:,1], fault.trace[:,2], "k-", lw=0.3)
-#end
-#quiver(centroids_lon, centroids_lat, centroids_ve, centroids_vn, color="C1", scale=300)
-#quiver(vlon, vlat, pve, pvn, color="r", scale=300)
-#quiver(vlon, vlat, rve, rvn, color="b", scale=300)
-#axis("equal")
-#
-#
-#
-#figure(figsize=(5, 9))
-## suptitle("Observed vs. Modeled Quaternary Slip Rates")
-#
-#subplot(2,1,1)
-#data_max = maximum([maximum(dex_geol_obs), maximum(dex_geol_pred)])
-#data_min = minimum([minimum(dex_geol_obs), minimum(dex_geol_pred)])
-#
-#plot([data_min, data_max], [data_min, data_max], "C1--", lw=0.5)
-#
-#axis("equal")
-#errorbar(dex_geol_obs, dex_geol_pred, xerr=dex_geol_err, yerr=dex_geol_pred_err,
-#         fmt=",", elinewidth=0.3)
-#
-#autoscale(false)
-#
-#fill_between([data_min, 0., -data_min], 
-#             [data_min, 0., data_min], 
-#             [data_min, data_min, data_min],
-#             color="grey",
-#             lw=0.,
-#             alpha=0.1)
-#
-#fill_between([data_min, 0., -data_min], 
-#             [-data_min, -data_min, -data_min],
-#             [-data_min, 0., -data_min], 
-#             color="grey",
-#             lw=0.,
-#             alpha=0.1)
-#
-#xlabel("observed")
-#ylabel("modeled")
-#title("dextral")
-#
-#subplot(2,1,2)
-#
-#data_max = maximum([maximum(ext_geol_obs), maximum(ext_geol_pred)])
-#data_min = minimum([minimum(ext_geol_obs), minimum(ext_geol_pred)])
-#plot([data_min, data_max], [data_min, data_max], "C1--", lw=0.5)
-#
-#axis("equal")
-#errorbar(ext_geol_obs, ext_geol_pred, xerr=ext_geol_err, yerr=ext_geol_pred_err, 
-#         fmt=",", elinewidth=0.3)
-#
-#autoscale(false)
-#
-#fill_between([data_min, 0., -data_min], 
-#             [data_min, 0., data_min], 
-#             [data_min, data_min, data_min],
-#             color="grey",
-#             lw=0.,
-#             alpha=0.1)
-#
-#fill_between([data_min, 0., -data_min], 
-#             [-data_min, -data_min, -data_min],
-#             [-data_min, 0., -data_min], 
-#             color="grey",
-#             lw=0.,
-#             alpha=0.1)
-#
-#
-#xlabel("observed")
-#ylabel("modeled")
-#title("extension")
-#
-#tight_layout()
-#
-#if save_results == true
-#    savefig("../../../china_faults_paper/figs/fault_rates.pdf")
-#end
-#
-#show()
+map_fig = Oiler.Plots.plot_results_map(results, vel_groups, faults)
+rates_fig = Oiler.Plots.plot_slip_rate_fig(geol_slip_rate_df, 
+                                           geol_slip_rate_vels, 
+                                           fault_df, results)
+
+if save_results == true
+    savefig("../../../china_faults_paper/figs/fault_rates.pdf")
+end
+
+show()
 
 Oiler.WebViewer.write_web_viewer(results=results, block_df=block_df,
                                  ref_pole="1111", directory="../web_viewer")
