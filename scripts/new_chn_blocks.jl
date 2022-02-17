0a6e8cb9eab26036307ce9e9c258122c7172bf29
itchy <richard.h.styron@gmail.com>
2022-02-17 14:32:54 -0800

    updated with results for paper

--- scripts/new_chn_blocks.jl
+++ scripts/new_chn_blocks.jl
@@ -5,25 +5,27 @@ using Oiler
 using JSON
 using CSV
 using DataFrames, DataFramesMeta
-using ArchGDAL
-const AG = ArchGDAL
+#using ArchGDAL
+#const AG = ArchGDAL
 
-using PyPlot
+plots = true
+
+if plots == true
+    using PyPlot
+end
 
 save_results = true
 
 
+# load data
 fault_file = "../block_data/chn_faults.geojson"
 gnss_vels_file = "../block_data/gnss_vels.geojson"
-# load data
 
 # faults
-
 data_gpkg = "../block_data/chn_faults_blocks.gpkg"
 geol_slip_rates_file = "../block_data/geol_slip_rate_pts.geojson"
 block_file = "../block_data/chn_blocks.geojson"
 
-#fault_df = Oiler.IO.gis_vec_file_to_df(fault_file)
 block_df = Oiler.IO.gis_vec_file_to_df(block_file)
 block_df[!,:fid] = string.(block_df[!,:fid])
 
@@ -32,16 +34,6 @@ gnss_df_all = Oiler.IO.gis_vec_file_to_df(gnss_vels_file)
 println("n blocks: ", size(block_df, 1))
 
 fault_weight = 2.
-
-#faults = [Oiler.IO.row_to_fault(fault_df[i,:], name=fault_df[i,:name] )
-#for i in 1:size(fault_df, 1)]
-#
-#fault_vels_ = map(Oiler.fault_to_vels, faults);
-#fault_vels = reduce(vcat, fault_vels_)
-#
-#geol_slip_rate_vels = Oiler.IO.make_geol_slip_rate_vels(geol_slip_rate_df, 
-#    fault_df; err_return_val=5.)
-
 
 fault_df, faults, fault_vels = Oiler.IO.process_faults_from_gis_files(
                                                         fault_file,
@@ -57,17 +49,12 @@ geol_slip_rate_df, geol_slip_rate_vels = Oiler.IO.make_geol_slip_rate_vels!(
 
 println("n fault slip rate vels: ", length(geol_slip_rate_vels))
 
-
-
-
-
 gnss_vels = Oiler.IO.make_vels_from_gnss_and_blocks(gnss_df_all, block_df;
     ve=:e_vel, vn=:n_vel, ee=:e_err, en=:n_err, name=:station,
     fix="1111"
 )
 
 vels = vcat(fault_vels, gnss_vels, geol_slip_rate_vels);
-# vels = vcat(fault_vels, gnss_vels);
 
 println("n gnss vels: ", length(gnss_vels))
 
@@ -100,48 +87,48 @@ if save_results == true
     name="China fault slip rates")
 end
 
-block_centroids = [AG.centroid(block_df[i, :geometry]) 
-                   for i in 1:size(block_df, 1)]
-
-pole_arr = collect(values(results["poles"]))
-pole_arr = [pole for pole in pole_arr if typeof(pole) == Oiler.PoleCart]
-
-centroids_lon = []
-centroids_lat = []
-centroids_ve = []
-centroids_vn = []
-centroids_ee = []
-centroids_en = []
-centroids_cen = []
-eur_rel_poles = Array{Oiler.PoleCart}(undef, size(block_centroids, 1))
-for (i, b_cent) in enumerate(block_centroids)
-    bc_lon = AG.getx(b_cent, 0)
-    bc_lat =  AG.gety(b_cent, 0)
-    push!(centroids_lon, bc_lon)
-    push!(centroids_lat, bc_lat)
-    # PvGb = Oiler.BlockRotations.build_PvGb_deg(bc_lon, bc_lat)
-    
-    pole = Oiler.Utils.get_path_euler_pole(pole_arr, "1111", 
-                                           string(block_df[i, :fid]))
-    
-    # ve, vn, vu = PvGb * [pole.x, pole.y, pole.z]
-    block_vel = Oiler.BlockRotations.predict_block_vel(bc_lon, bc_lat, pole)
-    push!(centroids_ve, block_vel.ve)
-    push!(centroids_vn, block_vel.vn)
-    push!(centroids_ee, block_vel.ee)
-    push!(centroids_en, block_vel.en)
-    push!(centroids_cen, block_vel.cen)
-    eur_rel_poles[i] = pole
-end
-
-centroids_pred_df = DataFrame()
-centroids_pred_df.lon = round.(centroids_lon, digits=5)
-centroids_pred_df.lat = round.(centroids_lat, digits=5)
-centroids_pred_df.ve =  round.(centroids_ve, digits=3)
-centroids_pred_df.vn =  round.(centroids_vn, digits=3)
-centroids_pred_df.ee =  round.(centroids_ee, digits=3)
-centroids_pred_df.en =  round.(centroids_en, digits=3)
-centroids_pred_df.cen = round.(centroids_cen, digits=3)
+#block_centroids = [AG.centroid(block_df[i, :geometry]) 
+#                   for i in 1:size(block_df, 1)]
+#
+#pole_arr = collect(values(results["poles"]))
+#pole_arr = [pole for pole in pole_arr if typeof(pole) == Oiler.PoleCart]
+#
+#centroids_lon = []
+#centroids_lat = []
+#centroids_ve = []
+#centroids_vn = []
+#centroids_ee = []
+#centroids_en = []
+#centroids_cen = []
+#eur_rel_poles = Array{Oiler.PoleCart}(undef, size(block_centroids, 1))
+#for (i, b_cent) in enumerate(block_centroids)
+#    bc_lon = AG.getx(b_cent, 0)
+#    bc_lat =  AG.gety(b_cent, 0)
+#    push!(centroids_lon, bc_lon)
+#    push!(centroids_lat, bc_lat)
+#    # PvGb = Oiler.BlockRotations.build_PvGb_deg(bc_lon, bc_lat)
+#    
+#    pole = Oiler.Utils.get_path_euler_pole(pole_arr, "1111", 
+#                                           string(block_df[i, :fid]))
+#    
+#    # ve, vn, vu = PvGb * [pole.x, pole.y, pole.z]
+#    block_vel = Oiler.BlockRotations.predict_block_vel(bc_lon, bc_lat, pole)
+#    push!(centroids_ve, block_vel.ve)
+#    push!(centroids_vn, block_vel.vn)
+#    push!(centroids_ee, block_vel.ee)
+#    push!(centroids_en, block_vel.en)
+#    push!(centroids_cen, block_vel.cen)
+#    eur_rel_poles[i] = pole
+#end
+#
+#centroids_pred_df = DataFrame()
+#centroids_pred_df.lon = round.(centroids_lon, digits=5)
+#centroids_pred_df.lat = round.(centroids_lat, digits=5)
+#centroids_pred_df.ve =  round.(centroids_ve, digits=3)
+#centroids_pred_df.vn =  round.(centroids_vn, digits=3)
+#centroids_pred_df.ee =  round.(centroids_ee, digits=3)
+#centroids_pred_df.en =  round.(centroids_en, digits=3)
+#centroids_pred_df.cen = round.(centroids_cen, digits=3)
 
 
 if save_results == true
@@ -155,16 +142,18 @@ if save_results == true
                                            
 end
 
-map_fig = Oiler.Plots.plot_results_map(results, vel_groups, faults)
-rates_fig = Oiler.Plots.plot_slip_rate_fig(geol_slip_rate_df, 
-                                           geol_slip_rate_vels, 
-                                           fault_df, results)
-
-if save_results == true
-    savefig("../../../china_faults_paper/figs/fault_rates.pdf")
+if plots == true
+    map_fig = Oiler.Plots.plot_results_map(results, vel_groups, faults)
+    rates_fig = Oiler.Plots.plot_slip_rate_fig(geol_slip_rate_df, 
+                                               geol_slip_rate_vels, 
+                                               fault_df, results)
+    
+    if save_results == true
+        savefig("../../../china_faults_paper/figs/fault_rates.pdf")
+    end
+    
+    show()
 end
-
-show()
 
 Oiler.WebViewer.write_web_viewer(results=results, block_df=block_df,
                                  ref_pole="1111", directory="../web_viewer")
