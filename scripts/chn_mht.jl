a12e7ba2d8370d985b59647f927f701cde73bca9
Richard Styron <richard.h.styron@gmail.com>
2026-04-20 11:53:45 -0700

    added mht script and prelim s asia bounds

---
+++ scripts/chn_mht.jl
@@ -0,0 +1,159 @@
+using Revise
+
+using Oiler
+
+using JSON
+using CSV
+using DataFrames, DataFramesMeta
+using PyPlot
+
+save_results = false
+
+fault_weight = 2.
+
+chn_fault_file = "../block_data/chn_faults.geojson"
+cea_fault_file = "../../c_asia_blocks/block_data/c_asia_faults.geojson"
+gnss_vels_file = "../block_data/gnss_vels.geojson"
+tibet_vel_field_file = "../geod_data/tibet_insar_vels_2023_04.geojson"
+mht_tris_file = "../../subduction/sub_tri_meshes/mht_rhs_20_km.geojson"
+bound_file = "../block_data/s_asia_bounds.geojson"
+# load data
+
+# faults
+geol_slip_rates_file = "../block_data/geol_slip_rate_pts.geojson"
+block_file = "../block_data/chn_blocks.geojson"
+c_asia_block_file = "../../c_asia_blocks/block_data/c_asia_blocks.geojson"
+
+
+chn_block_df = Oiler.IO.gis_vec_file_to_df(block_file)
+cea_block_df = Oiler.IO.gis_vec_file_to_df(c_asia_block_file)
+block_df = vcat(chn_block_df, cea_block_df, cols=:union)
+block_df = Oiler.IO.get_blocks_in_bounds!(block_df, 
+                            Oiler.IO.gis_vec_file_to_df(bound_file))
+
+
+println("n blocks: ", size(block_df, 1))
+
+
+fault_df, faults, fault_vels = Oiler.IO.process_faults_from_gis_files(
+                                                        chn_fault_file,
+                                                        cea_fault_file,
+                                                        fid_drop=[
+                                                            7,
+                                                            8,
+                                                            11,
+                                                            12,
+                                                            13,
+                                                            14,
+                                                            15,
+                                                            16,
+                                                            17,
+                                                            18,
+                                                            19,
+                                                            20,
+                                                            240,
+                                                            1,
+                                                            ],
+                                                        block_df=block_df)
+fault_df[!,:fid] = string.(fault_df[!,:fid])
+println("n faults: ", length(faults))
+println("n faults vels: ", length(fault_vels))
+
+geol_slip_rate_df = Oiler.IO.gis_vec_file_to_df(geol_slip_rates_file)
+geol_slip_rate_df, geol_slip_rate_vels = Oiler.IO.make_geol_slip_rate_vels!(
+                                                    geol_slip_rate_df,
+                                                    fault_df)
+
+println("n fault slip rate vels: ", length(geol_slip_rate_vels))
+
+
+@info "doing non-fault block boundaries"
+@time non_fault_bounds = Oiler.IO.get_non_fault_block_bounds(block_df, faults)
+bound_vels = vcat(map(b->Oiler.Boundaries.boundary_to_vels(b, ee=2.0, en=2.0), 
+                      non_fault_bounds)...)
+
+
+gnss_df = Oiler.IO.gis_vec_file_to_df(gnss_vels_file)
+gnss_vels = Oiler.IO.make_vels_from_gnss_and_blocks(gnss_df, block_df;
+    ve=:e_vel, vn=:n_vel, ee=:e_err, en=:n_err, name=:station,
+    fix="1111"
+)
+
+@info " doing comet insar vels"
+tibet_vel_field_df = Oiler.IO.gis_vec_file_to_df(tibet_vel_field_file)
+tibet_vel_field_df[!,"station"] = map(x->join(["tibet_insar_", x]),
+                                      string.(tibet_vel_field_df[!,:fid]))
+
+@time tibet_vel_field_vels = Oiler.IO.make_vels_from_gnss_and_blocks(
+    tibet_vel_field_df, block_df; 
+    name=:station, fix="1111")
+
+#subsample
+#tibet_vel_field_vels = tibet_vel_field_vels[1:10:end]
+@info "$(length(tibet_vel_field_vels)) Tibet vels"
+
+println("n gnss vels: ", length(gnss_vels))
+
+# tris
+tri_json = JSON.parsefile(mht_tris_file)
+tris = Oiler.IO.tris_from_geojson(tri_json)
+
+
+
+vels = vcat(fault_vels, 
+            gnss_vels, 
+            tibet_vel_field_vels, 
+            geol_slip_rate_vels);
+
+
+vel_groups = Oiler.group_vels_by_fix_mov(vels);
+
+
+# solve
+results = Oiler.solve_block_invs_from_vel_groups(vel_groups; faults=faults,
+tris=tris,
+                                               regularize_tris=true,
+                                               tri_distance_weight=10.,
+                                               predict_vels=true,
+                                               check_nans=true,
+                                               pred_se=false)
+
+# look at outputs
+Oiler.ResultsAnalysis.get_block_centroid_vels(results, block_df; fix="1111")
+Oiler.ResultsAnalysis.compare_data_results(results=results,
+                                           vel_groups=vel_groups,
+                                           geol_slip_rate_df=geol_slip_rate_df,
+                                           geol_slip_rate_vels=geol_slip_rate_vels,
+                                           fault_df=fault_df)
+
+println(results["stats_info"])
+
+
+poles = results["poles"]
+
+if save_results == true
+    Oiler.IO.write_fault_results_to_gj(results, 
+    "../results/chn_faults_out.geojson",
+    name="China fault slip rates")
+
+    Oiler.IO.write_block_centroid_vels_to_csv(results;
+                                        outfile="../results/block_vels_velfield.csv",
+                                        fix="1111")
+    Oiler.IO.write_gnss_vel_results_to_csv(results, vel_groups,
+                                        name="../results/chn_gnss_results_velfield.csv")
+                                           
+end
+
+map_fig = Oiler.Plots.plot_results_map(results, vel_groups, faults)
+rates_fig = Oiler.Plots.plot_slip_rate_fig(geol_slip_rate_df, 
+                                           geol_slip_rate_vels, 
+                                           fault_df, results)
+
+if save_results == true
+    savefig("../../../china_faults_paper/figs/fault_rates_velfield.pdf")
+end
+
+show()
+
+Oiler.WebViewer.write_web_viewer(results=results, block_df=block_df,
+                                 ref_pole="1111", directory="../web_viewer")
